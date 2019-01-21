7ffdc735066628ba9970aef07f22bd5e2f34deb7
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-01-21 19:47:16 +0100

    Add wpf server project

---
+++ Exercise.SignalR.Server/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Exercise.SignalR.Server
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand StartCommmand { get; }
+
+        public ICommand StopCommand { get; set; }
+    }
+}
