e584ec106270001c3a2277aae42c4244ec4dd94f
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2019-01-27 09:58:22 +0100

    Add room to messageHub

--- Exercise.SignalR.Server/MessageHub.cs
+++ Exercise.SignalR.Server/MessageHub.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Exercise.SignalR.Server
@@ -10,29 +11,39 @@ namespace Exercise.SignalR.Server
     /// </summary>
     public class MessageHub : Hub
     {
-        private static List<string> Users = new List<string>();
-
+        // Connection ID and name
+        private static readonly Dictionary<string,string> Users = new Dictionary<string,string>();
+        private static readonly Dictionary<string, HashSet<string>> Rooms = new Dictionary<string, HashSet<string>>();
         public void SignIn(string name)
         {
-            Users.Add(name);
+            Users[Context.ConnectionId] = name;
 
             App.Messager.Send<string>($"User {name} connected");
 
-            Clients.All.OnUserChanged(Users);
-        }
-
-        public void SignOut(string name)
-        {
-            Users.Remove(name);
-
-            App.Messager.Send<string>($"User {name} disconnected");
-
-            Clients.All.OnUserChanged(Users);
+            Clients.All.OnUserChanged(Users.Select(a => a.Value).ToList());
         }
 
         public void Send(string name, string message)
         {
             Clients.All.AddMessage(name, message);
+        }
+
+        public async Task JoinRoom(string roomName)
+        {
+            await Groups.Add(Context.ConnectionId, roomName);
+
+            Rooms[roomName].Add(Users[Context.ConnectionId]);
+
+            Clients.All.UserJoinRoom(Users[Context.ConnectionId]);
+        }
+
+        public async Task LeaveRoom(string roomName)
+        {
+            await Groups.Remove(Context.ConnectionId, roomName);
+
+            Rooms[roomName].Remove(Users[Context.ConnectionId]);
+
+            Clients.All.UserLeaveRoom(Users[Context.ConnectionId]);
         }
 
         public override Task OnConnected()
@@ -46,11 +57,15 @@ namespace Exercise.SignalR.Server
         {
             App.Messager.Send<string>("Client disconnected: " + Context.ConnectionId);
 
+            Users.Remove(Context.ConnectionId);
+
+            Clients.All.OnUserChanged(Users.Select(a => a.Value).ToList());
+
             return base.OnDisconnected(stopCalled);
         }
         public override async Task OnReconnected()
         {
-            await base.OnReconnected();    
+            await base.OnReconnected();
         }
     }
 }
