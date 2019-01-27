51223991a0d7b1e939bf061f9b781a829e6c6534
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2019-01-27 10:28:16 +0100

    add sign in logic to the message-hub

--- Exercise.SignalR.Server/MessageHub.cs
+++ Exercise.SignalR.Server/MessageHub.cs
@@ -12,20 +12,31 @@ namespace Exercise.SignalR.Server
     public class MessageHub : Hub
     {
         // Connection ID and name
-        private static readonly Dictionary<string,string> Users = new Dictionary<string,string>();
+        private static readonly Dictionary<string, string> Users = new Dictionary<string,string>();
         private static readonly Dictionary<string, HashSet<string>> Rooms = new Dictionary<string, HashSet<string>>();
-        public void SignIn(string name)
+
+        public async Task SignIn(string name)
         {
             Users[Context.ConnectionId] = name;
 
             App.Messager.Send<string>($"User {name} connected");
 
-            Clients.All.OnUserChanged(Users.Select(a => a.Value).ToList());
+            await JoinRoom("Main room");
+
+            var userRooms = Users.Select(a => new { Name = a.Value, Rooms = Rooms[a.Key] });
+
+            foreach (var user in userRooms)
+            {
+                foreach (var room in user.Rooms)
+                {
+                    Clients.Caller.UserJoinRoom(room, user);
+                }
+            }
         }
 
-        public void Send(string name, string message)
+        public void Send(string name, string room, string message)
         {
-            Clients.All.AddMessage(name, message);
+            Clients.Group(room).AddMessage(name, room, message);
         }
 
         public async Task JoinRoom(string roomName)
@@ -34,7 +45,7 @@ namespace Exercise.SignalR.Server
 
             Rooms[roomName].Add(Users[Context.ConnectionId]);
 
-            Clients.All.UserJoinRoom(Users[Context.ConnectionId]);
+            Clients.All.UserJoinRoom(roomName, Users[Context.ConnectionId]);
         }
 
         public async Task LeaveRoom(string roomName)
@@ -43,7 +54,7 @@ namespace Exercise.SignalR.Server
 
             Rooms[roomName].Remove(Users[Context.ConnectionId]);
 
-            Clients.All.UserLeaveRoom(Users[Context.ConnectionId]);
+            Clients.All.UserLeaveRoom(roomName, Users[Context.ConnectionId]);
         }
 
         public override Task OnConnected()
